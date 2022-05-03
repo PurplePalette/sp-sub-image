c10fdcf51146fb9c82a904259de93ee2f7a5003b
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2022-05-03 19:50:31 +0900

    Refactor: Rewrite with FastAPI
    Closes: #1 #2 #3 #4 #5 #6 #7 #9

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.8
+
+ENV PYTHONUNBUFFERED 1
+
+EXPOSE 8000
+WORKDIR /app
+
+RUN apt install ca-certificates
+COPY poetry.lock pyproject.toml ./
+RUN pip install poetry==1.0.* && \
+    CURL_CA_BUNDLE="" && \
+    poetry config virtualenvs.create false && \
+    poetry install --no-dev
+
+COPY . ./
+
+CMD uvicorn --host=0.0.0.0 main:app
--- main.py
+++ main.py
@@ -1,45 +1,77 @@
-from flask import Flask, request, make_response, send_file
+import hashlib
 import os
-from PIL import Image
+import traceback
 from io import BytesIO
-import numpy as np
 
-app = Flask(__name__)
+import boto3
+import botocore
+import botocore.exceptions
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from PIL import Image
+from pydantic import BaseModel
 
-@app.route('/', methods=['POST'])
-def segment():
-    # Pillowに変換 
-    img = Image.open(BytesIO(request.data)).convert('RGB')
-    #色の抽出
-    small_img = img.resize((100,200))
-    color_arr = np.array(small_img)
-    w_size, h_size, n_color = color_arr.shape
-    color_arr = color_arr.reshape(w_size * h_size, n_color)
-    r = [elem[0] for elem in color_arr]
-    g = [elem[1] for elem in color_arr]
-    b = [elem[2] for elem in color_arr]
+load_dotenv()
 
-    def expand(pil_img):
-      background_color = (int(np.median(r)), int(np.median(g)), int(np.median(b)))
-      width, height = pil_img.size
-      if width == height:
-          return pil_img
-      elif width > height:
-          result = Image.new(pil_img.mode, (width, width), background_color)
-          result.paste(pil_img, (0, (width - height) // 2))
-          return result
-      else:
-          result = Image.new(pil_img.mode, (height, height), background_color)
-          result.paste(pil_img, ((height - width) // 2, 0))
-          return result
 
-    ret_img = expand(img)
+class ConvertParams(BaseModel):
+    hash: str
 
-    img_io = BytesIO()
-    ret_img.save(img_io, 'PNG', quality=95)
-    img_io.seek(0)
 
-    response = make_response(send_file(img_io, mimetype='image/png'))
-    return response
-    
-app.run(host='0.0.0.0', port=8080)
+app = FastAPI()
+s3_endpoint = os.getenv("S3_ENDPOINT")
+s3 = boto3.resource(
+    "s3", endpoint_url=s3_endpoint, aws_access_key_id=os.getenv("S3_KEY"), aws_secret_access_key=os.getenv("S3_SECRET")
+)
+s3_bucket = s3.Bucket(os.getenv("S3_BUCKET"))
+
+
+@app.exception_handler(Exception)
+async def exception_handler(_request: Request, exc: Exception):
+    traceback.print_exception(type(exc), exc, exc.__traceback__)
+    return JSONResponse(status_code=500, content={"code": "internal_server_error", "message": "Internal server error"})
+
+
+@app.get("/")
+async def get_root():
+    return {
+        "status": "ok",
+    }
+
+
+@app.post("/convert")
+async def post_convert(item: ConvertParams):
+    object = s3_bucket.Object("LevelCover/" + item.hash)
+    try:
+        body = object.get()["Body"]
+    except botocore.exceptions.ClientError as e:
+        if e.response["Error"]["Code"] == "NoSuchKey":
+            raise HTTPException(404, {"status": "file_not_found", "message": "File not found"})
+        else:
+            raise
+    image = Image.open(body)
+    if image.size[0] == image.size[1]:
+        processed_image = image.resize((512, 512))
+    else:
+        color = image.resize((1, 1)).getpixel((0, 0))
+        base = Image.new("RGB", (512, 512), color)
+        if image.size[0] > image.size[1]:
+            resized = image.resize((512, round(512 * (image.size[1] / image.size[0]))))
+        else:
+            resized = image.resize((round(512 * (image.size[0] / image.size[1])), 512))
+        base.paste(resized, (0, (512 - resized.size[1]) // 2))
+        processed_image = base
+    image_io = BytesIO()
+    processed_image.save(image_io, format="PNG")
+    image_io.seek(0)
+
+    image_hash = hashlib.sha1(image_io.getvalue()).hexdigest()
+    s3_bucket.put_object(
+        Key="LevelCover/" + image_hash,
+        Body=image_io.getvalue(),
+        ContentType="image/png",
+    )
+    return {
+        "hash": image_hash,
+    }
